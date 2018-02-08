52da0433302b2c1831c01fb8ad9ddf1b31cb7096
David Lewis and Ron Craig <ron@>
2018-02-08 11:02:29 -0800

    change coin_calculator method to drop down coin buckets

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,14 +1,19 @@
 class Coins
+  def convert change
+    (change * 100).to_i
+  end
 
-def coin_calculator(change)
+  def coin_calculator change
+    money = convert(change)
+    quarters = money / 25
+    money -= 25 * quarters
+    dimes = money / 10
+    money -= 10 * dimes
+    nickels = money / 5
+    money -= 5 * nickels
+    pennies = money
 
-    coin = (change * 100).to_i
-    "#{coin} pennies"
-end
+    "#{quarters} q #{dimes} d #{nickels} n #{pennies} p"
 
-
-
-
-
-
+  end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -5,7 +5,23 @@ require('coin_combination')
 describe("#coin_calculator") do
   it("Takes in an amount of change and returns the amount in pennies") do
     coin = Coins.new()
-    expect(coin.coin_calculator(0.20)).to(eq("20 pennies"))
+    expect(coin.coin_calculator(0.04)).to(eq("0 q 0 d 0 n 4 p"))
+  end
+  it("Takes in an amount of change and returns the amount in nickels") do
+    coin = Coins.new()
+    expect(coin.coin_calculator(0.05)).to(eq("0 q 0 d 1 n 0 p"))
+  end
+  it("Takes in an amount of change and returns the amount in nickels and pennies") do
+    coin = Coins.new()
+    expect(coin.coin_calculator(0.07)).to(eq("0 q 0 d 1 n 2 p"))
+  end
+  it("Takes in an amount of change and returns the amount in pennies, nickels, and dimes") do
+    coin = Coins.new()
+    expect(coin.coin_calculator(0.17)).to(eq("0 q 1 d 1 n 2 p"))
+  end
+  it("Takes in an amount of change and returns the amount in pennies, nickels, dimes, and quarters") do
+    coin = Coins.new()
+    expect(coin.coin_calculator(0.42)).to(eq("1 q 1 d 1 n 2 p"))
   end
 
 end
