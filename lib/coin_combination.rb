a0ac14080a7eda49a176606b8901f5659e5bc0d6
Ron Craig <ron.craig@comcast.net>
2018-09-24 00:27:25 -0700

    add new line constant, remove whitespace, edit coin_calculator method

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,19 +1,21 @@
+
+NEW_LINE = "\n"
+
 class Coins
-  def convert change
-    (change * 100).to_i
+  def initialize(input)
+    @input = input
   end
 
-  def coin_calculator change
-    money = convert(change)
-    quarters = money / 25
-    money -= 25 * quarters
-    dimes = money / 10
-    money -= 10 * dimes
-    nickels = money / 5
-    money -= 5 * nickels
-    pennies = money
-
-    "#{quarters} q #{dimes} d #{nickels} n #{pennies} p"
-
+  def coin_calculator
+    coins = @input
+    quarters = coins / 25
+    coins -= 25 * quarters
+    dimes = coins / 10
+    coins -= 10 * dimes
+    nickels = coins / 5
+    coins -= 5 * nickels
+    pennies = coins
+    puts NEW_LINE + "#{quarters} q #{dimes} d #{nickels} n #{pennies} p" + NEW_LINE
+    quarters.to_s + ' q ' + dimes.to_s + ' d ' + nickels.to_s + ' n ' + pennies.to_s + ' p'
   end
 end
