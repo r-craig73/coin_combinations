6b4bfde5fbd69c11969e382f79c5ef5437f78c07
Ron Craig <ron.craig@comcast.net>
2018-09-24 00:25:52 -0700

    edit specs rules, remove the coin_calculator method argument

--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -2,26 +2,29 @@ require('rspec')
 require('pry')
 require('coin_combination')
 
-describe("#coin_calculator") do
-  it("Takes in an amount of change and returns the amount in pennies") do
-    coin = Coins.new()
-    expect(coin.coin_calculator(0.04)).to(eq("0 q 0 d 0 n 4 p"))
-  end
-  it("Takes in an amount of change and returns the amount in nickels") do
-    coin = Coins.new()
-    expect(coin.coin_calculator(0.05)).to(eq("0 q 0 d 1 n 0 p"))
-  end
-  it("Takes in an amount of change and returns the amount in nickels and pennies") do
-    coin = Coins.new()
-    expect(coin.coin_calculator(0.07)).to(eq("0 q 0 d 1 n 2 p"))
-  end
-  it("Takes in an amount of change and returns the amount in pennies, nickels, and dimes") do
-    coin = Coins.new()
-    expect(coin.coin_calculator(0.17)).to(eq("0 q 1 d 1 n 2 p"))
-  end
-  it("Takes in an amount of change and returns the amount in pennies, nickels, dimes, and quarters") do
-    coin = Coins.new()
-    expect(coin.coin_calculator(0.42)).to(eq("1 q 1 d 1 n 2 p"))
+describe('coin_calculator') do
+  it('returns the amount in pennies') do
+    coin = Coins.new(4)
+    expect(coin.coin_calculator).to(eq('0 q 0 d 0 n 4 p'))
   end
 
+  it('returns the amount in nickels') do
+    coin = Coins.new(5)
+    expect(coin.coin_calculator).to(eq('0 q 0 d 1 n 0 p'))
+  end
+
+  it('returns the amount in nickels and pennies') do
+    coin = Coins.new(7)
+    expect(coin.coin_calculator).to(eq('0 q 0 d 1 n 2 p'))
+  end
+
+  it('returns the amount in pennies, nickels, and dimes') do
+    coin = Coins.new(17)
+    expect(coin.coin_calculator).to(eq('0 q 1 d 1 n 2 p'))
+  end
+
+  it('returns the amount in pennies, nickels, dimes, and quarters') do
+    coin = Coins.new(42)
+    expect(coin.coin_calculator).to(eq('1 q 1 d 1 n 2 p'))
+  end
 end
